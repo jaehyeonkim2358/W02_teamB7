f3c1318b2c22760e8cb86e61e6f6b88338417058
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-09-29 23:53:06 +0900

    1920_kjh 46492kb 480ms

--- 1920/1920_kjh.py
+++ 1920/1920_kjh.py
@@ -1,4 +1,5 @@
 n = int(input())
+# 이분 탐색을 위해, 탐색할 리스트는 정렬되어있어야 한다.
 A = sorted(list(map(int, input().split())))
 
 m = int(input())
@@ -8,11 +9,15 @@ def binary_search(key, start, end, target):
     s = start
     e = end
     while s < e:
-        mid = (s + e) // 2
-        if target[mid] >= key:
-            e = mid
+        # mid = (s + e) // 2
+        mid = (s + e) // 2 + 1
+        # if target[mid] >= key:
+        if target[mid] > key:
+            # e = mid
+            e = mid - 1
         else:
-            s = mid+1
+            # s = mid + 1
+            s = mid
     print(int(target[e]==key))
 
 
