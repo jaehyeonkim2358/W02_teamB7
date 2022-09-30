2dad4d503b08f8b5d25ac5d68b05185fd3fafe4c
rivolt0421 <dlrkddnr0421@daum.net>
2022-09-30 20:57:35 +0900

    10828_lkw 108944kb 2068ms

--- 18258/18258_lkw.py
+++ 18258/18258_lkw.py
@@ -3,27 +3,32 @@ import sys
 sys.stdin = open("18258/input.txt","r")
 
 n = int(input())
-stk = deque([],maxlen=10000)
+queue = deque()
 for _ in range(n):
     cmd = sys.stdin.readline().split()
     
     if cmd[0] == 'push':
-        stk.append(int(cmd[1]))
+        queue.append(int(cmd[1]))
         
     elif cmd[0] == 'pop':
         try:
-            print(stk.pop())
+            print(queue.popleft())
+        except IndexError:
+            print(-1)
+
+    elif cmd[0] == 'front':
+        try:
+            print(queue[0])
+        except IndexError:
+            print(-1)
+    elif cmd[0] == 'back':
+        try:
+            print(queue[-1])
         except IndexError:
             print(-1)
 
     elif cmd[0] == 'size':
-        print(len(stk))
+        print(len(queue))
 
     elif cmd[0] == 'empty':
-        print(int(not stk))
-
-    elif cmd[0] == 'top':
-        try:
-            print(stk[-1])
-        except IndexError:
-            print(-1)
+        print(int(not queue))
