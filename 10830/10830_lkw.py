f24a3ef81d0d8ced384c5d492669c21cf5e7c1e5
rivolt0421 <dlrkddnr0421@daum.net>
2022-10-03 20:34:24 +0900

    10830_lkw 30840kb 68ms

--- 10830/10830_lkw.py
+++ 10830/10830_lkw.py
@@ -4,10 +4,10 @@ sys.stdin = open("10830/input.txt","r")
 n, B = map(int, input().split())
 arr=[]
 for _ in range(n):
-    arr.append(list(map(lambda x: int(x) % 1000, sys.stdin.readline().split())))
-
-threshole = 100
-memo =[None] * (threshole + 1)
+    arr.append(list(map(lambda x: int(x) % 1000, sys.stdin.readline().split())))    # 원본 배열도 1000으로 나눈 나머지로 저장한다.
+                                                                                    # 뒤에서 memo[1] = arr 로 사용하기 때문.
+threshold = 100
+memo =[None] * (threshold + 1)
 
 def multiply(X, Y, n):
     result = []
@@ -22,16 +22,17 @@ def multiply(X, Y, n):
     return result
 
 def go(arr, B, memo, n):
-    if B <= threshole:
+    if B <= threshold:
         return memo[B]
 
     h = B//2
     X = go(arr, h, memo, n)
-    Y = X if B%2 == 0 else multiply(X, memo[1], n)
+    Y = X if B%2 == 0 else multiply(X, memo[1], n)  # 어차피 원본 행렬은 같기 때문에 B가 짝수면 X와 Y가 같을 것이고, 홀수면 Y에 원본행렬을 한번만 더 곱해주면 된다.
+                                                    # B//2 를 통해 반으로 나눴을 때 왼쪽 분기의 값이 도출되면, 오른쪽 분기는 생성할 필요가 없다.
     return multiply(X, Y, n)
 
 memo[1] = arr
-for i in range(2, min(threshole+1, B+1)):
+for i in range(2, min(threshold+1, B+1)):
     memo[i] = multiply(arr, memo[i-1], n)
 
 for line in go(arr, B, memo, n):
